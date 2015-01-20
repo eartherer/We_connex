8897a95aa301cb64424ff507dcf20932c17a6165
Earther <Earther@Earthers-MacBook-Air.local>
2015-01-20 13:01:26 +0700

    Initial Commit

---
+++ We_connex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  We_connex
+//
+//  Created by Earther on 20/01/2015.
+//  Copyright (c) 2015 Earther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ We_connex/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  We_connex
+//
+//  Created by Earther on 20/01/2015.
+//  Copyright (c) 2015 Earther. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
